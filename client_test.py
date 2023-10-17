7a825bf4fb431d82ca95118f59cc03f22fe98f2c
Abdulmalik <mackquadrizz@gmail.com>
2023-10-17 17:30:31 +0100

    Task 1 submission

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    self.assertEqual(getDataPoint(quotes[0]), ('ABC', 120.48, 121.2, 120.84), "***********TEST FAILED**********")
+    self.assertEqual(getDataPoint(quotes[1]), ('DEF', 117.87, 121.68, 119.775), "***********TEST FAILED**********")
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +17,13 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    self.assertEqual(getDataPoint(quotes[0]), ('ABC', 120.48, 119.2, 119.84), "***********TEST FAILED**********")
+    self.assertEqual(getDataPoint(quotes[1]), ('DEF', 117.87, 121.68, 119.775), "***********TEST FAILED**********")
 
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatioWithZeroPrices(self):
+    self.assertEqual(getRatio(121.6, 0), (None), "***********TEST FAILED**********")
 
 
 
